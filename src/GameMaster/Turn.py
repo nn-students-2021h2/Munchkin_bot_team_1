c3fb7571a527458e00c63b2ecc136d024dedfdf9
Dmitry Shirocov <shirocov.dmitry@gmail.com>
2021-12-20 23:12:07 +0300

    turn class skeleton

---
+++ src/GameMaster/Turn.py
@@ -0,0 +1,21 @@
+from Battle import Battle
+
+""" 
+a class that describes the behavior of the game during combat
+
+"""
+
+
+class Turn:
+    def __init__(self, player, taken_card):
+        self._player = player
+        self._taken_card = taken_card
+
+    def turn(self):
+        if self._taken_card.type == "monster":
+            # init Fight class
+            Battle()
+        elif self._taken_card.type == "curse":
+            # apply a curse on the current player
+            self._player.make_curse()  # method in Players class
+
