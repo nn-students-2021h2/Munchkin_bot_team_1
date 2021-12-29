2aa757e85086b62044a6ea2b530859a85c646f56
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2021-12-30 01:42:22 +0300

    Remove Card

--- src/Cards/base_card.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-
-class BaseCard:
-    def __init__(self, name: str, description: str, deck_type: Any, play_time: Any):
-        self.name = name
-        self.description = description
-        self.deck_type = deck_type
-        self.play_time = play_time
